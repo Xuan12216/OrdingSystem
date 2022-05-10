f1be4bf35c9088a35d326fdbd51839fcc10de9a0
GOMUNDER <58372831+GOMUNDER@users.noreply.github.com>
2022-05-10 10:19:24 +0800

    update README.md update member table 更新US-022 bottom 功能

--- app/src/main/java/com/example/foodordering/First_activity.java
+++ app/src/main/java/com/example/foodordering/First_activity.java
@@ -2,15 +2,23 @@ package com.example.foodordering;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -21,7 +29,12 @@ import com.google.firebase.database.ValueEventListener;
 
 public class First_activity extends AppCompatActivity implements View.OnClickListener
 {
+    BottomNavigationView bottomNavigationView;
     private Button logout;
+    home home=new home();
+    search search=new search();
+    cart cart=new cart();
+    account account=new account();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -31,8 +44,31 @@ public class First_activity extends AppCompatActivity implements View.OnClickLis
         logout = (Button) findViewById(R.id.logOut);
 
         logout.setOnClickListener(this);
-    }
 
+        bottomNavigationView=findViewById(R.id.bottommenu);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment,home).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.homemenu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment,home).commit();
+                        return true;
+                    case R.id.searchmenu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment,search).commit();
+                        return true;
+                    case R.id.cartmenu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment,cart).commit();
+                        return true;
+                    case R.id.accountmenu:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment,account).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
     @Override
     public void onClick(View view)
     {
