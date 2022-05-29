2f5157d28e68c99db8b7ff9763269c2d89a768b8
XUAN <azx990524123@gmail.com>
2022-05-29 16:30:50 +0800

    Restaurant Backkey

--- app/src/main/java/com/example/foodordering/Restaurant.java
+++ app/src/main/java/com/example/foodordering/Restaurant.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class Restaurant extends AppCompatActivity {
+public class Restaurant extends AppCompatActivity implements View.OnClickListener {
     private String []foodname= {"KFC XL set", "KFC family set", "KFC egg tart", "coca cola", "sevenup"};
     private String []foodprice={"200$","400$","180$","50$","50$"};
     private int []foodimg={R.drawable.kfcxlset,R.drawable.kfcfamilyset,R.drawable.eggcake,R.drawable.cocacola,R.drawable.sevenup};
@@ -27,8 +27,19 @@ public class Restaurant extends AppCompatActivity {
         ImageView restaurantimg=findViewById(R.id.restaurantImg);
         ImageView back=findViewById(R.id.backkey);
 
+        back.setOnClickListener(this);
         RestaurantAdapter restaurantAdapter =new RestaurantAdapter(getApplicationContext(),foodimg,foodname,foodprice);
         foodlist.setAdapter(restaurantAdapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.backkey:
+                finish();
+                break;
+        }
     }
 
     /*
